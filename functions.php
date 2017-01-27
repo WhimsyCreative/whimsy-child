6bffce42f749abc15de3d59f2aa429cc9397b41e
natashacozad <hello@whimsycreative.co>
2017-01-27 14:25:34 -0600

    Initial release

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'whimsy_child_scripts_styles', 15 );
+add_action( 'wp_enqueue_scripts', 'whimsy_child_scripts_styles', 95 );
 
 /*
  * Additional style and script file inclusion.
@@ -10,12 +10,12 @@ function whimsy_child_scripts_styles() {
     /**
      * Include new Google fonts
      */
-    require_once get_stylesheet_directory() . '/library/inc/fonts.php';
-    wp_enqueue_style( 'whimsy-fonts', whimsy_child_fonts_url(), array(), null );
+    require_once get_stylesheet_directory() . '/child/inc/fonts.php';
+    wp_enqueue_style( 'whimsy-child-fonts', whimsy_child_fonts_url(), array(), null );
     
     /**
      * Include custom.css file last for persistent style overrides.
      */
-    wp_enqueue_style( 'whimsy-style', get_stylesheet_directory_uri() . 'custom.css', '1.0.0', true, 400 );
+    wp_enqueue_style( 'whimsy-child-custom-styles', get_stylesheet_directory_uri() . 'custom.css', '1.0.0', true, 400 );
 
 }
