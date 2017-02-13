e58b89eeb6a5e7e56ce7a20c73a88f514e32ad3a
Natasha Cozad <hello@whimsycreative.co>
2017-02-13 12:17:35 -0600

    1.0.1

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'whimsy_child_scripts_styles', 15 );
+add_action( 'wp_enqueue_scripts', 'whimsy_child_scripts_styles', 2 );
+add_action( 'setup_theme', 'whimsy_child_unhook_whimsy_framework_functions', 2 );
 
 /*
  * Additional style and script file inclusion.
@@ -10,12 +11,20 @@ function whimsy_child_scripts_styles() {
     /**
      * Include new Google fonts
      */
-    require_once get_stylesheet_directory() . '/library/inc/fonts.php';
-    wp_enqueue_style( 'whimsy-fonts', whimsy_child_fonts_url(), array(), null );
+    require_once get_stylesheet_directory() . '/child/inc/fonts.php';
+    wp_enqueue_style( 'whimsy-child-fonts', whimsy_child_fonts_url(), array(), null );
     
     /**
      * Include custom.css file last for persistent style overrides.
      */
-    wp_enqueue_style( 'whimsy-style', get_stylesheet_directory_uri() . 'custom.css', '1.0.0', true, 400 );
+    wp_enqueue_style( 'whimsy-child-style', get_stylesheet_directory_uri() . '/custom.css', '1.0.0', true, 400 );
 
+}
+
+
+/*
+ * Change the load order for the menu to place it after site title
+ */
+function whimsy_child_unhook_whimsy_framework_functions() {
+    remove_action( 'whimsy_nav_inside_before', 'whimsy_responsive_nav', 40 );
 }
